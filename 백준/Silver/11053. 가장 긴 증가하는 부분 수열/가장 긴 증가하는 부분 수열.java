abf1f03455d9c9e5d8f806b34b6bbee0c810c7cc
han ho sung <79980357+hosunghan-0821@users.noreply.github.com>
2022-05-31 13:35:49 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 128 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -7,26 +7,47 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N =Integer.parseInt(br.readLine());
         int [] arr = new int [N];
-        int [] dp = new int[N];
+        int [] LIS = new int[N];
         StringTokenizer st =new StringTokenizer(br.readLine()," ");
-        int i=0;
+        int k=0;
         while(st.hasMoreTokens()){
             
-            arr[i]=Integer.parseInt(st.nextToken());
-            i++;
-            
+            arr[k]=Integer.parseInt(st.nextToken());
+            k++;
         }
+        //0번에 일단 값 대입하고 시작
+        LIS[0]=arr[0];
+       
+        // 아이디어 값을 차례대로 작은 값을 넣는다. 만약 이전 값보다 작다면, 이진탐색 lower bound를 통해 
+        // 가까운 수 들중 첫번째로 큰 수와 교체한다.
+        // 이 방식은 수열의 길이를 구할 때만 유효하다.
         
-        for(i=0;i<arr.length;i++){
-            dp[i]=1;
-            for(int j=0;j<i;j++){
-                
-                if( arr[i]>arr[j] && dp[i]<dp[j]+1 ){
-                    dp[i]=dp[j]+1;
+         int LISLength = 1;
+        
+        for(int i=1;i<N;i++){
+            int key = arr[i];
+            //원하는대로 증가하는 수열일 경우. 
+            if(LIS[LISLength-1]<key){
+                LIS[LISLength]=key;
+                LISLength++;
+            }
+            //아닐 경우 대치 이분탐색을 통해 근처값들 중 
+            else{
+                int lo=0;
+                int hi=LISLength;
+                //lowerbound를 통해서.
+                while(lo<hi){
+                    int mid = (lo+hi)/2;
+                    if(key<=LIS[mid]){
+                        hi=mid;
+                    }
+                    else{
+                        lo=mid+1;
+                    }
                 }
+                LIS[lo]=key;
             }
         }
-        Arrays.sort(dp);
-        System.out.println(dp[N-1]);
+        System.out.println(LISLength);
     }
 }
