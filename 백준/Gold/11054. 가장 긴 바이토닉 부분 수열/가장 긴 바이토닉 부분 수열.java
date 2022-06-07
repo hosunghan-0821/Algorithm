1e571a0d0f080709f139d27e4baa74a281d9d3a8
han ho sung <79980357+hosunghan-0821@users.noreply.github.com>
2022-06-07 12:05:22 +0900

    [Gold III] Title: 가장 긴 바이토닉 부분 수열, Time: 176 ms, Memory: 14912 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -2,73 +2,68 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
+
+public class Main{
+    static int[] dp, dpR;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        arr = new int[N];
+        dp = new int[N];
+        dpR = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        //Top-Down 재귀 방식 LIS 탐색
+        //Top-Down 재귀방식 LDS 탐색
+        for (int i = 0; i < N; i++) {
+            LIS(i);
+            LDS(i);
+
+        }
+        int max = -1;
+        for (int i = 0; i < N; i++) {
+            max = Math.max(dp[i] + dpR[i], max);
+        }
+        System.out.println(max - 1);
+
+
+    }
+
+    //Top-Down 재귀 방식 LIS 탐색
+    static int LIS(int N) {
+        //1로 초기화
+        if (dp[N] == 0) {
+            dp[N] = 1;
+        
+        for (int i = N - 1; i >= 0; i--) {
+            if (arr[N] > arr[i]) {
+                dp[N] = Math.max(LIS(i) + 1, dp[N]);
+            }
+          }
+        }
+        return dp[N];
+    }
+    //Top-Down 재귀방식 LDS 탐색
+
+    static int LDS(int N) {
+        if (dpR[N] == 0) {
+            dpR[N] = 1;
+        
+            
+        for (int i = N + 1; i < dpR.length; i++) {
+            if (arr[N] > arr[i]) {
+                dpR[N] = Math.max(LDS(i) + 1, dpR[N]);
+            }
+        }
+       }     
+        return dpR[N];
+    }
  
-public class Main {
- 
-	static Integer[] r_dp;
-	static Integer[] l_dp;
-	static int[] seq;
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		int N = Integer.parseInt(br.readLine());
- 
-		r_dp = new Integer[N];	// LIS dp
-		l_dp = new Integer[N];	// LDS dp
-		seq = new int[N];
- 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
- 
-		for (int i = 0; i < N; i++) {
-			seq[i] = Integer.parseInt(st.nextToken());
-		}
- 
-		for (int i = 0; i < N; i++) {
-			LIS(i);
-			LDS(i);
-		}
- 
-		int max = -1;
-		for (int i = 0; i < N; i++) {
-			max = Math.max(r_dp[i] + l_dp[i], max);
-		}
-		System.out.println(max - 1);
- 
-	}
- 
-	static int LIS(int N) {
- 
-		// 만약 탐색하지 않던 위치의 경우
-		if (r_dp[N] == null) {
-			r_dp[N] = 1; // 1로 초기화
- 
-			// N 이전의 노드들을 탐색
-			for (int i = N - 1; i >= 0; i--) {
-				// 이전의 노드 중 seq[N]의 값보다 작은 걸 발견했을 경우
-				if (seq[i] < seq[N]) {
-					r_dp[N] = Math.max(r_dp[N], LIS(i) + 1);
-				}
-			}
-		}
-		return r_dp[N];
-	}
- 
-	static int LDS(int N) {
-		
-		// 만약 탐색하지 않던 위치의 경우
-		if (l_dp[N] == null) {
-			l_dp[N] = 1; // 1로 초기화
- 
-			// N 이후의 노드들을 탐색
-			for (int i = N + 1; i < l_dp.length; i++) {
-				// 이후의 노드 중 seq[N]의 값보다 작은 걸 발견했을 경우
-				if (seq[i] < seq[N]) {
-					l_dp[N] = Math.max(l_dp[N], LDS(i) + 1);
-				}
-			}
-		}
-		return l_dp[N];
-	}
 }
