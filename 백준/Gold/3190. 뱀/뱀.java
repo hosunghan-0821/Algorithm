87909e04df37972ebaabd4081988fdb3ef86ca80
han ho sung <79980357+hosunghan-0821@users.noreply.github.com>
2022-08-09 21:08:40 +0900

    [Gold IV] Title: 뱀, Time: 136 ms, Memory: 14416 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -11,15 +11,17 @@ public class Main {
     public static int[][] map;
     public static Deque<Point> deque = new ArrayDeque <Point>();
 
-    public static int[] dy = {0,1,0,-1};
-    public static int[] dx = {1,0,-1,0};
+    public static int[] dy = {1,0,-1,0};
+    public static int[] dx = {0,1,0,-1};
 
     public  static char[] r = new char[10001];
     public static void main(String[] args) throws IOException{
         int res = 0 ;
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // NXN 정사각형
         N= Integer.parseInt(br.readLine());
+        // 사과 개수
         K= Integer.parseInt(br.readLine());
 
         map = new int[N][N];
@@ -27,36 +29,42 @@ public class Main {
 
         for(int i = 0 ; i < K;i++){
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int col = Integer.parseInt(st.nextToken());
             int row = Integer.parseInt(st.nextToken());
-           
+            int col = Integer.parseInt(st.nextToken());
 
-            map[col-1][row-1]=1;
+            map[row-1][col-1]=1;
         }
+        //명령어 개수
         L =Integer.parseInt(br.readLine());
 
         for(int i = 0 ; i < L ;i++){
             StringTokenizer st = new StringTokenizer(br.readLine()," ");
             int t = Integer.parseInt(st.nextToken());
             //초에 방향전환정보 넣는거구나.
+            //해당하는 초에 방향전환 정보 삽입
             r[t]= st.nextToken().charAt(0);
         }
 
         int dir=0;
         while(true){
             res++;
-            int y = deque.getLast().y + dy[dir];
             int x = deque.getLast().x + dx[dir];
+            int y = deque.getLast().y + dy[dir];
+
             Point p = new Point(x,y);
 
             if(y<0 || y>=N || x<0 || x>= N || deque.contains(p)){
                 break;
             }
 
+            //머리 한칸 전진
             deque.add(p);
-            if(map[y][x]!=1) deque.removeFirst();
-            else map[y][x]=0;
+            //사과가 없다면 꼬리 제거
+            if(map[x][y]!=1) deque.removeFirst();
+            //사과가 있다면 꼬리 유지
+            else map[x][y]=0;
 
+            //방향전환관련
             if(r[res]=='D') dir = (dir+1)%4;
             if(r[res]=='L') dir = (dir+3)%4;
         }
