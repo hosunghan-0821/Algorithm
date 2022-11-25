b7875e8c30e1038b3f71262740f50a6afef4741f
han ho sung <79980357+hosunghan-0821@users.noreply.github.com>
2022-11-25 14:56:07 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.22 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,57 +1,58 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        
-        ArrayList<Integer> lottos_arr = new ArrayList<>();
-        ArrayList<Integer> win_nums_arr = new ArrayList<>();
-        
-        for(int i = 0 ; i <lottos.length;i++){
-            lottos_arr.add(lottos[i]);
-            win_nums_arr.add(win_nums[i]);
-        }
-        Collections.sort(lottos_arr);
-        Collections.sort(win_nums_arr);
-        int count = 0 ;
-        int one_eyedJack=0;
-        for(int i = 0 ; i< lottos_arr.size(); i++){
-            if(win_nums_arr.contains(lottos_arr.get(i))){
-                count++;
-            }
-            if(lottos_arr.get(i).equals(0)){
-                one_eyedJack++;
-            }
-        }
-        System.out.println(count);
-        System.out.println(one_eyedJack);
-        int min_rank = count;
-        int max_rank = count+one_eyedJack;
         int[] answer = new int[2];
-        answer[0] = check_rank(max_rank);
-        answer[1] = check_rank(min_rank);
-        return answer;
-    }
-    public int check_rank(int count){
-        int rank = 0;
-        switch(count){
-            case 6:
-                rank = 1;
+        ArrayList<Integer> lotto = new ArrayList<>();
+        ArrayList<Integer> win_num = new ArrayList<>();
+        // ArrayList 로 변경
+        for(int i = 0 ; i< lottos.length;i++){
+            lotto.add(lottos[i]);
+            win_num.add(win_nums[i]);
+        }
+        Collections.sort(lotto);
+        Collections.sort(win_num);
+        
+        int zero_num=0;
+        int min_rank=0;
+        int contain_num=0;
+        for(int i = 0 ; i <6 ; i ++){
+        
+            if(lotto.get(i)==0){
+                
+                zero_num++;
+                continue;
+            }
+            
+            if(win_num.contains(lotto.get(i))){
+                
+                contain_num++;
+            }
+            
+        }
+        switch(contain_num){
+            case  2:
+                min_rank=5;
                 break;
-            case 5:
-                rank = 2;
+            case  3:
+                min_rank=4;
                 break;
-            case 4:
-                rank = 3;
+            case  4:
+                min_rank=3;
+                  break;
+            case  5:
+                min_rank=2;
+                  break;
+            case  6:
+                min_rank=1;
                 break;
-            case 3:
-                rank= 4;
-                break;
-            case 2:
-                rank = 5;
-                break;
-            default :
-                rank =6;
+            default : 
+                min_rank=6;
                 break;
         }
-        return rank;
+        if(zero_num==6) answer[0]=1;
+            else answer[0] = min_rank-zero_num;
+    
+        answer[1] = min_rank;
+        return answer;
     }
 }
