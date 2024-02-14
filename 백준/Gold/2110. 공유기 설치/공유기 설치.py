b5cee9dcb1a217b291d64cf3187760c017bd4caa
han ho sung <79980357+hosunghan-0821@users.noreply.github.com>
2024-02-14 11:52:34 +0900

    [Gold IV] Title: 공유기 설치, Time: 576 ms, Memory: 38848 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,34 +1,38 @@
-n, c = map(int, input().split())
+import sys
 
-array = []
-for i in range(n):
-    array.append(int(input()))
+input = sys.stdin.readline
 
-array.sort()
+N, C = map(int, input().split())
 
-
-def binary_search(array, start, end):
-    while start <= end:
-        mid = (start + end) // 2
-        current = array[0]
-        count = 1
-
-        for i in range(1, len(array)):
-            if array[i] >= current + mid:
-                count += 1
-                current = array[i]
-
-        if count >= c:
-            global answer
-            start = mid + 1
-            answer = mid
-        else:
-            end = mid - 1
-
+data = [int(input()) for i in range(N)]
+data.sort()
 
 start = 1
-end = array[-1] - array[0]
-answer = 0
+end = data[N - 1] - data[0]
 
-binary_search(array, start, end)
-print(answer)
+ans = 0
+# for문을 활용한 2진 탐색
+while start <= end:
+    mid = (start + end) // 2
+
+    cnt = 1
+    sum = 0
+    current = data[0]
+
+    for i in range(N - 1):
+        if data[i + 1] >= current + mid:
+            cnt += 1
+            current = data[i + 1]
+
+    if cnt >= C:
+        start = mid + 1
+        ans = mid
+
+    else:
+        end = mid - 1
+
+
+
+print(ans)
+
+
