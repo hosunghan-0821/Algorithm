cc305ffa50e57c1a23686d9f360f08ac48bdff5e
han ho sung <79980357+hosunghan-0821@users.noreply.github.com>
2024-03-03 10:57:53 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 703.91 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
@@ -0,0 +1,16 @@
+import math
+def solution(r1, r2):
+    answer = 0
+    temp = 0
+    
+    for i in range(1, r2 + 1):
+        if i > r1:
+            minH = 0
+        else :
+            minH = math.ceil(math.sqrt(r1**2 - i**2))
+        
+        maxH = math.floor(math.sqrt(r2**2 - i **2))
+        
+        answer += maxH - minH + 1
+        
+    return answer * 4
