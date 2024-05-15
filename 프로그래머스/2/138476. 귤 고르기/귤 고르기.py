4ca28bebe120061ae9a650d909ca7c7bc4fb6eb0
han ho sung <79980357+hosunghan-0821@users.noreply.github.com>
2024-05-16 08:40:18 +0900

    [level 2] Title: 귤 고르기, Time: 40.18 ms, Memory: 25.7 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,22 +1,3 @@
-import heapq
 def solution(k, tangerine):
     answer = 0
-    count_dict = dict()
-    data = []
-    for tangerine_each in tangerine:
-        if count_dict.get(tangerine_each) is not None:
-            count_dict[tangerine_each] = count_dict[tangerine_each] + 1
-        else :
-            count_dict[tangerine_each] = 1
- 
-
-    count_dict = dict(sorted(count_dict.items(), key = lambda x: x[1], reverse =True))
-
-    for i in count_dict:
-        if k <= 0:
-            break
-        k -= count_dict[i]
-        answer += 1
-    
-    
     return answer
